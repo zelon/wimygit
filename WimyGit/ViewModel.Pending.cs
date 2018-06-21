cfaba12df8b3c7283bf7276923f7fd17cd5a9fe5
KIM, JINWOOK <zelonion@gmail.com>
2018-06-22 00:12:50 +0900

    Extract FileStatus

--- WimyGit/ViewModel.Pending.cs
+++ WimyGit/ViewModel.Pending.cs
@@ -18,32 +18,8 @@ namespace WimyGit
         public ICommand RevertCommand { get; private set; }
         public ICommand OpenExplorerSelectedFileCommand { get; private set; }
         public ICommand OpenSelectedFileCommand { get; private set; }
+
         private string commit_message_;
-
-        public class FileStatus
-        {
-            private ViewModel view_model_;
-            private bool is_selected_ = false;
-
-            public string Status { get; set; }
-            public string FilePath { get; set; }
-            public string Display { get; set; }
-            public bool IsSelected
-            {
-                get { return is_selected_; }
-                set
-                {
-                    is_selected_ = value;
-                    view_model_.StageSelectedCommand.RaiseCanExecuteChanged();
-                    view_model_.StageSelectedPartialCommand.RaiseCanExecuteChanged();
-                }
-            }
-
-            public FileStatus(ViewModel view_model)
-            {
-                view_model_ = view_model;
-            }
-        }
 
         public System.Collections.ObjectModel.ObservableCollection<FileStatus> ModifiedList { get; set; }
         public System.Collections.ObjectModel.ObservableCollection<FileStatus> StagedList { get; set; }
