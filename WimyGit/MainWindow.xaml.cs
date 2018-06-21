482a46807585e4ca48caf02983e4578686839adc
KIM, JINWOOK <zelonion@gmail.com>
2018-06-22 02:12:00 +0900

    Add 'PlusTab' button

--- WimyGit/MainWindow.xaml.cs
+++ WimyGit/MainWindow.xaml.cs
@@ -14,6 +14,27 @@ namespace WimyGit
             InitializeComponent();
 
             RestoreTabs();
+            AddPlusTabButton();
+        }
+
+        private void AddTab(string path)
+        {
+            var path_list = path.Split(Path.DirectorySeparatorChar);
+            string tab_title = path_list[path_list.Length - 1];
+
+            TabItem tab_item = new TabItem();
+            var tab_header = new UserControls.RepositoryTabHeader();
+            tab_header.Title.Content = tab_title;
+            tab_header.CloseButton.Click += (sender, e) =>
+            {
+                System.Diagnostics.Debug.WriteLine("close tab");
+                tab_control_.Items.Remove(tab_item);
+            };
+            tab_item.Header = tab_header;
+            tab_item.Content = new RepositoryTab(path);
+            tab_item.Width = 200;
+
+            tab_control_.Items.Insert(0, tab_item);
         }
 
         private void RestoreTabs()
@@ -21,21 +42,7 @@ namespace WimyGit
             int count = 0;
             foreach (var path in Service.GetInstance().recent_repository_.GetList())
             {
-                var path_list = path.Split(Path.DirectorySeparatorChar);
-                string tab_title = path_list[path_list.Length - 1];
-
-                TabItem tab_item = new TabItem();
-                var tab_header = new UserControls.RepositoryTabHeader();
-                tab_header.Title.Content = tab_title;
-                tab_header.CloseButton.Click += (sender, e) =>
-                {
-                    System.Diagnostics.Debug.WriteLine("close tab");
-                    tab_control_.Items.Remove(tab_item);
-                };
-                tab_item.Header = tab_header;
-                tab_item.Content = new RepositoryTab(path);
-
-                tab_control_.Items.Insert(0, tab_item);
+                AddTab(path);
 
                 ++count;
                 if (count == 3)
@@ -43,6 +50,22 @@ namespace WimyGit
                     break;
                 }
             }
+        }
+
+        private void AddPlusTabButton()
+        {
+            var button = new Button();
+            button.Width = 50;
+            button.Content = "+";
+            button.Click += (sender, e) => {
+                AddTab("C:\\");
+            };
+
+            TabItem tab_item = new TabItem();
+            tab_item.Header = button;
+            tab_item.Width = button.Width + 12;
+            tab_item.Focusable = false;
+            tab_control_.Items.Insert(tab_control_.Items.Count, tab_item);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
