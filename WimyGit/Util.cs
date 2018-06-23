17717e47ed738ac8f9b3cb9ccbb670cd545ee774
KIM, JINWOOK <zelonion@gmail.com>
2018-06-24 03:22:35 +0900

    Add a assertion

--- WimyGit/Util.cs
+++ WimyGit/Util.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace WimyGit
 {
@@ -6,6 +7,8 @@ namespace WimyGit
     {
         public static string WrapFilePath(string filename)
         {
+            Debug.Assert(string.IsNullOrEmpty(filename) == false);
+
             if (filename.StartsWith("\""))
             {
                 return filename;
