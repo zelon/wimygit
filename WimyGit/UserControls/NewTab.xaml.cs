69ad5a63bb55915dc7d29d64746ff998319e32c5
KIM, JINWOOK <zelonion@gmail.com>
2018-06-24 02:29:18 +0900

    Support drag&drop into NewTab controls

--- WimyGit/UserControls/NewTab.xaml.cs
+++ WimyGit/UserControls/NewTab.xaml.cs
@@ -8,11 +8,31 @@ namespace WimyGit.UserControls
     /// </summary>
     public partial class NewTab : UserControl
     {
+        private Action<string> new_tab_result_;
+
         public NewTab(Action<string> new_tab_result)
         {
+            new_tab_result_ = new_tab_result;
+
             InitializeComponent();
 
             DataContext = new NewTabViewModel(new_tab_result);
+        }
+
+        private void Grid_DragOver(object sender, System.Windows.DragEventArgs e)
+        {
+            e.Effects = System.Windows.DragDropEffects.All;
+        }
+
+        private void Grid_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+            string[] paths = (string[])e.Data.GetData(System.Windows.DataFormats.FileDrop);
+
+            if (paths.Length != 1)
+            {
+                return;
+            }
+            new_tab_result_(paths[0]);
         }
     }
 }
