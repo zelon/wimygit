1070d3c7b275594ad7d05bdcc4b1b7ae32a80fb8
KIM, JINWOOK <zelonion@gmail.com>
2018-06-30 17:11:57 +0900

    Remove needless codes

--- WimyGit/ILogger.cs
+++ WimyGit/ILogger.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WimyGit {
-  interface ILogger {
-    void AddLog (string msg);
-  }
+﻿
+namespace WimyGit
+{
+    interface ILogger
+    {
+        void AddLog(string msg);
+    }
 }
