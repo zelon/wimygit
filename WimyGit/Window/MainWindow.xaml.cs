9e9cbdefdcbbcbab9b053c25b7f1d3be99b9d8b3
KIM, JINWOOK <zelonion@gmail.com>
2018-11-04 22:31:09 +0900

    Remove needless code in MainWindow.xaml.cs

--- WimyGit/Window/MainWindow.xaml.cs
+++ WimyGit/Window/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
-﻿using System.IO;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace WimyGit
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
