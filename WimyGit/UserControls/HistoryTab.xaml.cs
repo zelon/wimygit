04cc6db905a8ced80a1e3c0da3068540a11ca239
KIM, JINWOOK <zelonion@gmail.com>
2019-03-02 18:14:00 +0900

    Extract HistoryTab from RepositoryTab

---
+++ WimyGit/UserControls/HistoryTab.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Controls.Primitives;
+
+namespace WimyGit.UserControls
+{
+    public partial class HistoryTab : UserControl
+    {
+        public HistoryTab()
+        {
+            InitializeComponent();
+        }
+
+        private void HistoryList_ScrollChanged(object sender, RoutedEventArgs e)
+        {
+            List<ScrollBar> scrollBarList = GetVisualChildCollection<ScrollBar>(sender);
+            foreach (ScrollBar scrollBar in scrollBarList)
+            {
+                if (scrollBar.Orientation == Orientation.Vertical)
+                {
+                    if (scrollBar.Maximum > 0 && scrollBar.Value == scrollBar.Maximum)
+                    {
+                        ViewModel viewModel = (ViewModel)DataContext;
+                        if (viewModel.MoreHistoryCommand.CanExecute(sender))
+                        {
+                            viewModel.MoreHistoryCommand.Execute(sender);
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+
+        // http://stackoverflow.com/questions/4139341/wpf-listbox-onscroll-event
+        public static List<T> GetVisualChildCollection<T>(object parent) where T : Visual
+        {
+            List<T> visualCollection = new List<T>();
+            GetVisualChildCollection(parent as DependencyObject, visualCollection);
+            return visualCollection;
+        }
+        private static void GetVisualChildCollection<T>(DependencyObject parent, List<T> visualCollection) where T : Visual
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T)
+                {
+                    visualCollection.Add(child as T);
+                }
+                else if (child != null)
+                {
+                    GetVisualChildCollection(child, visualCollection);
+                }
+            }
+        }
+    }
+}
--- WimyGit/UserControls/RepositoryTab.xaml.cs
+++ WimyGit/UserControls/RepositoryTab.xaml.cs
@@ -2,7 +2,6 @@
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 
 namespace WimyGit
@@ -44,46 +43,6 @@ namespace WimyGit
 		private ViewModel GetViewModel()
 		{
 			return (ViewModel)this.DataContext;
-		}
-
-		private void HistoryList_ScrollChanged(object sender, RoutedEventArgs e)
-		{
-			List<ScrollBar> scrollBarList = GetVisualChildCollection<ScrollBar>(sender);
-			foreach (ScrollBar scrollBar in scrollBarList)
-			{
-				if (scrollBar.Orientation == Orientation.Vertical)
-				{
-					if (scrollBar.Maximum > 0 && scrollBar.Value == scrollBar.Maximum)
-					{
-						GetViewModel().MoreHistoryCommand.Execute(sender);
-						return;
-					}
-				}
-			}
-		}
-
-		// http://stackoverflow.com/questions/4139341/wpf-listbox-onscroll-event
-		public static List<T> GetVisualChildCollection<T>(object parent) where T : Visual
-		{
-			List<T> visualCollection = new List<T>();
-			GetVisualChildCollection(parent as DependencyObject, visualCollection);
-			return visualCollection;
-		}
-		private static void GetVisualChildCollection<T>(DependencyObject parent, List<T> visualCollection) where T : Visual
-		{
-			int count = VisualTreeHelper.GetChildrenCount(parent);
-			for (int i = 0; i < count; i++)
-			{
-				DependencyObject child = VisualTreeHelper.GetChild(parent, i);
-				if (child is T)
-				{
-					visualCollection.Add(child as T);
-				}
-				else if (child != null)
-				{
-					GetVisualChildCollection(child, visualCollection);
-				}
-			}
 		}
 
 		public void SelectAllUnstagedFilesListBox()
