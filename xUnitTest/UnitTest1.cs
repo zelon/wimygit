5dad5873ad8fd74043da220f3603750ea6256ba3
KIM, JINWOOK <zelonion@gmail.com>
2019-10-28 23:01:07 +0900

    Remove UnitTest from .NET Framework and Add xUnitTest

---
+++ xUnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using WimyGit;
+
+namespace xUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            string line = " M WimyGit.sln";
+            GitFileStatus gitFileStatus = GitPorcelainParser.ParseFileStatus(line);
+            Assert.Equal("WimyGit.sln", gitFileStatus.Modified.Filename);
+        }
+    }
+}
