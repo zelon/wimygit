ba56dbbc93633f2a1ba1661dae67eb0a69dc1dfb
KIM, JINWOOK <zelonion@gmail.com>
2019-12-06 22:13:38 +0900

    Add AddLog() to IGitRepository

--- WimyGit/IGitRepository.cs
+++ WimyGit/IGitRepository.cs
@@ -6,5 +6,6 @@ namespace WimyGit
         public GitWrapper GetGitWrapper();
         public System.Threading.Tasks.Task<bool> Refresh();
         public RunExternal CreateGitRunner();
+        public void AddLog(string message);
     }
 }
