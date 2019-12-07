94bb6eaf0986c59184358d7bac0ec1444cfb0816
KIM, JINWOOK <zelonion@gmail.com>
2019-12-07 15:30:55 +0900

    Add BoolToStringConverter to show 'Current' instead of 'True/False'

---
+++ WimyGit/Views/BranchTab/BoolToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WimyGit.UserControls
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public BoolToStringConverter()
+        {
+            TrueString = "True";
+            FalseString = "False";
+        }
+
+        public string TrueString { get; set; }
+        public string FalseString { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return TrueString;
+            }
+            return FalseString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
