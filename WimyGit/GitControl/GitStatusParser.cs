8f645177c49e13abf0fe9d948f16550bc45428cb
KIM, JINWOOK <zelonion@gmail.com>
2023-06-08 23:14:44 +0900

    Remove Unused using statements

--- WimyGit/GitControl/GitStatusParser.cs
+++ WimyGit/GitControl/GitStatusParser.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using WimyGitLib;
 
 namespace WimyGit
 {
--- WimyGit/GitControl/GitWrapper.cs
+++ WimyGit/GitControl/GitWrapper.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WimyGit
