60d6941be9fbc82df810ceb14617d7fbcb5ac4ca
KIM, JINWOOK <zelonion@gmail.com>
2026-05-07 23:21:56 +0900

    ci: set nightly app identifier in SetNightlyVersionSetting.ps1

--- cicd/SetNightlyVersionSetting.ps1
+++ cicd/SetNightlyVersionSetting.ps1
@@ -3,5 +3,6 @@ Copy-Item (Join-Path $PSScriptRoot ".." "wimygit-tauri" "src-tauri" "icons-night
 $tauriConfPath = Join-Path $PSScriptRoot ".." "wimygit-tauri" "src-tauri" "tauri.conf.json"
 $tauriConf = Get-Content $tauriConfPath -Raw | ConvertFrom-Json
 $tauriConf.productName = "Wimygit Nightly"
+$tauriConf.identifier = "com.wimysoft.wimygit.nightly"
 $tauriConf.app.windows[0].title = "WimyGit Nightly"
 $tauriConf | ConvertTo-Json -Depth 100 | Set-Content $tauriConfPath
