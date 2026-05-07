5657e9622883b71085ae05659a4fee7bd2cb442f
KIM, JINWOOK <zelonion@gmail.com>
2026-05-07 23:58:17 +0900

    ci: update nightly script to set APP_NAME in constants.ts

--- cicd/SetNightlyVersionSetting.ps1
+++ cicd/SetNightlyVersionSetting.ps1
@@ -1,8 +1,15 @@
 Copy-Item (Join-Path $PSScriptRoot ".." "wimygit-tauri" "src-tauri" "icons-nightly" "*") (Join-Path $PSScriptRoot ".." "wimygit-tauri" "src-tauri" "icons") -Recurse -Force
 
+$WIMYGIT_TITLE = "WimyGit Nightly"
+
 $tauriConfPath = Join-Path $PSScriptRoot ".." "wimygit-tauri" "src-tauri" "tauri.conf.json"
 $tauriConf = Get-Content $tauriConfPath -Raw | ConvertFrom-Json
 $tauriConf.productName = "Wimygit Nightly"
 $tauriConf.identifier = "com.wimysoft.wimygit.nightly"
-$tauriConf.app.windows[0].title = "WimyGit Nightly"
+$tauriConf.app.windows[0].title = $WIMYGIT_TITLE
 $tauriConf | ConvertTo-Json -Depth 100 | Set-Content $tauriConfPath
+
+$filePath = Join-Path $PSScriptRoot ".." "wimygit-tauri" "src" "constants.ts"
+$content = Get-Content $filePath -Raw
+$content = $content -replace 'APP_NAME\s*=\s*"WimyGit"', ('APP_NAME = "' + $WIMYGIT_TITLE + '"')
+Set-Content $filePath $content -Encoding UTF8
