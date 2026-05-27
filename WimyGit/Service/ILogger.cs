bde4691f311c0995a29bb6c438fc0fdc305767b9
KIM, JINWOOK <zelonion@gmail.com>
2026-05-27 23:14:57 +0900

    chore: clean up obsolete C# app code

--- WimyGit/Service/ILogger.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace WimyGit
-{
-	public interface ILogger
-	{
-		void AddLog(string msg);
-		void AddLog(System.Collections.Generic.List<string> msgs);
-	}
-}
--- WimyGitLib/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace WimyGitLib
-{
-    public static class Constants
-    {
-        public const string Untracked = "Untracked";
-    }
-}
